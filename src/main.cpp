86f70b46609836d4b1e3234f6de6abbc21a6fe0e
JaneStateDog <jedidiahswets@gmail.com>
2021-12-28 04:24:30 -0500

    Zooming

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 #include <map>
 
 
-#define GAME_WIDTH 128
-#define GAME_HEIGHT 128
+#define BASE_WIDTH 128
+#define BASE_HEIGHT 128
 
-#define WINDOW_WIDTH GAME_WIDTH * 3
-#define WINDOW_HEIGHT GAME_HEIGHT * 3
+#define WINDOW_WIDTH BASE_WIDTH * 3
+#define WINDOW_HEIGHT BASE_HEIGHT * 3
 
 
 typedef struct {
@@ -17,11 +17,18 @@ typedef struct {
       Uint8 G;
       Uint8 B;
       Uint8 A;
+
+      int size;
 } Pixel;
 
 
 int main() {
       srand(time(NULL));
+
+
+      int gameWidth = BASE_WIDTH;
+      int gameHeight = BASE_HEIGHT;
+      int zoom = 1;
 
 
       SDL_Event event;
@@ -31,11 +38,10 @@ int main() {
       SDL_Window *window = SDL_CreateWindow("pixel-logic", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
       
       SDL_Renderer *drawingRenderer = SDL_CreateRenderer(window, -1, 0);
-      SDL_RenderSetLogicalSize(drawingRenderer, GAME_WIDTH, GAME_HEIGHT);
+      SDL_RenderSetLogicalSize(drawingRenderer, gameWidth, gameHeight);
 
 
       int holding = 0;
-      int size = 1;
 
       std::map<std::pair<int, int>, Pixel> pixels;
 
@@ -57,8 +63,15 @@ int main() {
                         case SDL_MOUSEBUTTONUP: holding = 0; break;
 
                         case SDL_MOUSEWHEEL:
-                              if (event.wheel.y > 0) { size++; }
-                              else if (event.wheel.y < 0) { size--; }
+                              if (event.wheel.y < 0 && zoom < 14) { zoom++; }
+                              if (event.wheel.y > 0 && zoom > 1) { zoom--; }
+
+                              gameWidth = BASE_WIDTH * zoom;
+                              gameHeight = BASE_HEIGHT * zoom;
+
+                              std::cout << zoom << std::endl;
+
+                              SDL_RenderSetLogicalSize(drawingRenderer, gameWidth, gameHeight);
 
                               break;
 
@@ -69,18 +82,19 @@ int main() {
                   if (quit) { break; }
 
                   if (holding != 0) {
+                        int mouseX = round(event.button.x / zoom) * zoom;
+                        int mouseY = round(event.button.y / zoom) * zoom;
+
                         switch (holding) {
                               case SDL_BUTTON_LEFT:  {
-                                    Pixel pixel = {(Uint8)(std::rand() % 255), (Uint8)(std::rand() % 255), (Uint8)(std::rand() % 255), 255};
-                                    for (int x = 0; x < size; x++) for (int y = 0; y < size; y++) {
-                                          pixels[std::make_pair(event.button.x + x, event.button.y + y)] = pixel;
-                                    }
+                                    Pixel pixel = {(Uint8)(std::rand() % 255), (Uint8)(std::rand() % 255), (Uint8)(std::rand() % 255), 255, zoom};
+                                    pixels[std::make_pair(mouseX, mouseY)] = pixel;
                                     
                                     break;
                               }
 
                               case SDL_BUTTON_RIGHT:
-                                    pixels.erase(std::make_pair(event.button.x, event.button.y));
+                                    pixels.erase(std::make_pair(mouseX, mouseY));
                                     break;
                         }
                   }
@@ -90,10 +104,12 @@ int main() {
             // Draw
             for (auto &pair : pixels) {
                   std::pair<int, int> pos = pair.first;
-                  Pixel RGBA = pair.second;
+                  Pixel pixel = pair.second;
 
-                  SDL_SetRenderDrawColor(drawingRenderer, RGBA.R, RGBA.G, RGBA.B, RGBA.A);
-                  SDL_RenderDrawPoint(drawingRenderer, pos.first, pos.second);
+                  SDL_SetRenderDrawColor(drawingRenderer, pixel.R, pixel.G, pixel.B, pixel.A);
+
+                  SDL_Rect rect = {pos.first, pos.second, pixel.size, pixel.size};
+                  SDL_RenderFillRect(drawingRenderer, &rect);
             }
 
             SDL_RenderPresent(drawingRenderer);
